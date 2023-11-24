114482d60c58fbae0a0135de76fd552bf2878548
Marianaeiterer <marieiterer@gmail.com>
2023-11-24 11:49:30 -0700

    files updated with render link

--- routes/book.js
+++ routes/book.js
@@ -2,22 +2,37 @@ const express = require('express');
 const router = express.Router();
 
 const bookController = require('../controllers/books');
-const validation = require('../middleware/validate'); 
-const { isAuthenticate } = require('../middleware/authenticate'); 
+const validation = require('../middleware/validate');
+const { isAuthenticate } = require('../middleware/authenticate');
 
 //get all books
-router.get("/", bookController.getAllBooks);
+router.get('/', bookController.getAllBooks);
 
 //get book by ID
-router.get("/:id", bookController.getSingleBook);
+router.get('/:id', bookController.getSingleBook);
 
-//create new book 
-router.post("/", isAuthenticate, bookController.createBook);
+//create new book
+router.post(
+  '/',
+  isAuthenticate,
+  validation.saveBook,
+  bookController.createBook
+);
 
- //update book
- router.put("/:id", isAuthenticate, bookController.updateBook);
+//update book
+router.put(
+  '/:id',
+  isAuthenticate,
+  validation.saveBook,
+  bookController.updateBook
+);
 
 //delete book
-router.delete("/:id", isAuthenticate, bookController.deleteBook);
+router.delete(
+  '/:id',
+  isAuthenticate,
+  validation.saveBook,
+  bookController.deleteBook
+);
 
 module.exports = router;
--- routes/client.js
+++ routes/client.js
@@ -1,24 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
-const clientController = require("../controllers/clients");
-const validation = require('../middleware/validate'); 
-const { isAuthenticate } = require('../middleware/authenticate'); 
+const clientController = require('../controllers/clients');
+const validation = require('../middleware/validate');
+const { isAuthenticate } = require('../middleware/authenticate');
 
 //get all clients
 
-router.get("/", clientController.getAllClients);
+router.get('/', clientController.getAllClients);
 
 //get client by ID
-router.get("/:id", clientController.getSingleClient);
+router.get('/:id', clientController.getSingleClient);
 
-//create new client 
-router.post("/", isAuthenticate, clientController.createClient);
+//create new client
+router.post(
+  '/',
+  isAuthenticate,
+  validation.saveClient,
+  clientController.createClient
+);
 
 //update client
-router.put("/:id", isAuthenticate, clientController.updateClient);
+router.put(
+  '/:id',
+  isAuthenticate,
+  validation.saveClient,
+  clientController.updateClient
+);
 
 //delete client
-router.delete("/:id", isAuthenticate, clientController.deleteClient);
+router.delete(
+  '/:id',
+  isAuthenticate,
+  validation.saveClient,
+  clientController.deleteClient
+);
 
 module.exports = router;
